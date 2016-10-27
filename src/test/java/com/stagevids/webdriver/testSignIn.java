6afb60b3632769f960495ae2d5d294b10ea7ac2e
Roman Savchenko <r.sav4enko@gmail.com>
2016-10-27 11:35:50 +0300

    first test

---
+++ src/test/java/com/stagevids/webdriver/testSignIn.java
@@ -0,0 +1,58 @@
+package com.stagevids.webdriver;
+/**
+ * Created by Marina Repetilo on 27.10.16.
+ */
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class testSignIn {
+    private WebDriver driver;
+
+    @BeforeClass // Runs this method before the first test method in the current class is invoked
+    public void setUp() {
+        // Create a new instance of the Firefox driver
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void signInPassTest(){
+//        WebDriver driver = new FirefoxDriver();
+//        driver.navigate().to("https://qa.stagevids.com/");
+
+        driver.manage().window().maximize();
+        driver.get("https://qa.stagevids.com/");
+
+        driver.findElement(By.className("btn-login")).click();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("login_email")).sendKeys("marirepetilo1@gmail.com");
+
+        driver.findElement(By.xpath(".//*[@id='formModal1']/input[3]")).click();
+
+
+
+//        Assert.assertTrue("Error message!",
+//                driver.getTitle().startsWith("Stagevids"));
+
+
+
+    }
+
+    @AfterClass // Runs this method after all the test methods in the current class have been run
+    public void tearDown() {
+        // Close all browser windows and safely end the session
+        driver.close();
+        driver.quit();
+    }
+
+}
