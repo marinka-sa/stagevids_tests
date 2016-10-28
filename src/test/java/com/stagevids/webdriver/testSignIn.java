f39b63c8f4740055760e5199bf2fc18c99d6d2bb
Marina Repetilo <marirepetilo1@gmail.com>
2016-10-28 23:08:08 +0300

    tests for Sign In

--- src/test/java/com/stagevids/webdriver/testSignIn.java
+++ src/test/java/com/stagevids/webdriver/testSignIn.java
@@ -23,52 +23,152 @@ public class testSignIn {
 
     @BeforeClass // Runs this method before the first test method in the current class is invoked
     public void setUp() {
-        // Create a new instance of the Firefox driver
         driver = new FirefoxDriver();
     }
 
     @Test
-    public void signInPassTest()
-    {
-//        WebDriver driver = new FirefoxDriver();
-//        driver.navigate().to("https://qa.stagevids.com/");
-
+    public void notValidPasswordEmptyField() throws InterruptedException {
         driver.manage().window().maximize();
         driver.get("https://qa.stagevids.com/");
 
         driver.findElement(By.className("btn-login")).click();
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-//        driver.findElement(By.id("login_email")).sendKeys("marirepetilo1@gmail.com");
-        driver.findElement(By.id("login_email")).sendKeys("mari");
+        Thread.sleep(1000);
+        driver.findElement(By.id("login_email")).sendKeys("marirepetilo1@gmail.com");
 
         driver.findElement(By.xpath(".//*[@id='formModal1']/input[3]")).click();
-        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
 
-
-//        String errorMessage = driver.findElement(By.xpath(".//*[@id='formModal1']/div[2]")).getText();
-//        String errorMessage = driver.findElement(By.xpath(".//*[@id='formModal1']/div[1]")).getAttribute("innerHTML");
-
-//        String errorMessage = driver.findElement(By.cssSelector("#formModal1 > div:nth-child(4)")).getText();
-//        String errorMessage = driver.findElement(By.cssSelector("#formModal1 > div:nth-child(4)")).getAttribute("value");
-        WebElement errorMessage = driver.findElement(By.xpath(".//*[@id='formModal1']/div[1]"));
-//        String errorMessage = driver.findElement(By.className("dangerMessage")).getText();
+        Thread.sleep(1000);
+        WebElement errorMessage = driver.findElement(By.cssSelector("div[ng-show='errorMessageSignIn'"));
         System.err.println("!!!!!!!");
-        System.err.println(errorMessage);
-        Assert.assertTrue(errorMessage.getAttribute("textContent") == "you are awesome, but login or password is incorrect");
-
-
-//        Assert.assertTrue("Error message!",
-//               driver.getTitle().startsWith("you are awesome, but login or password is incorrect "));
+        System.err.println(errorMessage.getText());
+        System.err.println("!!!!!!!");
+        Assert.assertEquals(errorMessage.getText(), "you are awesome, but login or password is incorrect");
     }
+
+    @Test
+    public void notValidEmailField() throws InterruptedException {
+        driver.manage().window().maximize();
+        driver.get("https://qa.stagevids.com/");
+
+        driver.findElement(By.className("btn-login")).click();
+
+        Thread.sleep(1000);
+        driver.findElement(By.id("login_email")).sendKeys("ma");
+        driver.findElement(By.id("passsignin")).sendKeys("mari89");
+
+        driver.findElement(By.xpath(".//*[@id='formModal1']/input[3]")).click();
+
+        Thread.sleep(1000);
+        WebElement errorMessage = driver.findElement(By.cssSelector("div[ng-show='showValidMessageUserName'"));
+        System.err.println("!!!!!!!");
+        System.err.println(errorMessage.getText());
+        System.err.println("!!!!!!!");
+        Assert.assertEquals(errorMessage.getText(), "invalid sign in email");
+    }
+
+
+    @Test
+    public void emailEmptyField() throws InterruptedException {
+        driver.manage().window().maximize();
+        driver.get("https://qa.stagevids.com/");
+
+        driver.findElement(By.className("btn-login")).click();
+
+        Thread.sleep(1000);
+        driver.findElement(By.id("login_email")).sendKeys("");
+        driver.findElement(By.id("passsignin")).sendKeys("mari89");
+
+        driver.findElement(By.xpath(".//*[@id='formModal1']/input[3]")).click();
+
+        Thread.sleep(1000);
+        WebElement errorMessage = driver.findElement(By.cssSelector("div[ng-show='showValidMessageUserName'"));
+        System.err.println("!!!!!!!");
+        System.err.println(errorMessage.getText());
+        System.err.println("!!!!!!!");
+        Assert.assertEquals(errorMessage.getText(), "invalid sign in email");
+    }
+
+    @Test
+    public void notValidEmailField1() throws InterruptedException {
+        driver.manage().window().maximize();
+        driver.get("https://qa.stagevids.com/");
+
+        driver.findElement(By.className("btn-login")).click();
+
+        Thread.sleep(1000);
+        driver.findElement(By.id("login_email")).sendKeys("marirepetilo1@gmailcom");
+        driver.findElement(By.id("passsignin")).sendKeys("mari89");
+
+        driver.findElement(By.xpath(".//*[@id='formModal1']/input[3]")).click();
+
+        Thread.sleep(1000);
+        WebElement errorMessage = driver.findElement(By.cssSelector("div[ng-show='showValidMessageUserName'"));
+        System.err.println("!!!!!!!");
+        System.err.println(errorMessage.getText());
+        System.err.println("!!!!!!!");
+        Assert.assertEquals(errorMessage.getText(), "invalid sign in email");
+    }
+
+    @Test
+    public void notValidEmailField2() throws InterruptedException {
+        driver.manage().window().maximize();
+        driver.get("https://qa.stagevids.com/");
+
+        driver.findElement(By.className("btn-login")).click();
+
+        Thread.sleep(1000);
+        driver.findElement(By.id("login_email")).sendKeys("marirepetilo1gmail.com");
+        driver.findElement(By.id("passsignin")).sendKeys("mari89");
+
+        driver.findElement(By.xpath(".//*[@id='formModal1']/input[3]")).click();
+
+        Thread.sleep(1000);
+        WebElement errorMessage = driver.findElement(By.cssSelector("div[ng-show='showValidMessageUserName'"));
+        System.err.println("!!!!!!!");
+        System.err.println(errorMessage.getText());
+        System.err.println("!!!!!!!");
+        Assert.assertEquals(errorMessage.getText(), "invalid sign in email");
+    }
+
+    @Test
+    public void ValidLogIn() throws InterruptedException {
+        driver.manage().window().maximize();
+        driver.get("https://qa.stagevids.com/");
+
+        driver.findElement(By.className("btn-login")).click();
+
+        Thread.sleep(1000);
+        driver.findElement(By.id("login_email")).sendKeys("marirepetilo1@gmail.com");
+        driver.findElement(By.id("passsignin")).sendKeys("testov21");
+
+        driver.findElement(By.xpath(".//*[@id='formModal1']/input[3]")).click();
+        Thread.sleep(1000);
+        WebElement errorMessage = driver.findElement(By.cssSelector("div[ng-show='showValidMessageUserName'"));
+        System.err.println("!!!!!!!");
+        System.err.println(errorMessage.getText());
+        System.err.println("!!!!!!!");
+        Assert.assertEquals(errorMessage.getText(), "");
+        Thread.sleep(1000);
+        try {
+            driver.findElement(By.xpath(".//*[@id='navbar']/ul/li[5]/a/div/div[1]")).click();
+        }
+        catch (Exception e) {
+            System.err.println("Not Logged In!");
+        }
+//        driver.findElement(By.xpath(".//*[@id='navbar']/ul/li[5]/ul/li[2]/a")).click();
+
+
+    }
+
+
 
 
     @AfterClass // Runs this method after all the test methods in the current class have been run
     public void tearDown() {
         // Close all browser windows and safely end the session
-//        driver.close();
-//        driver.quit();
+        driver.close();
+        driver.quit();
     }
 
 }
